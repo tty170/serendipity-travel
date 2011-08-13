b5091bb92592a02887a2a50e9cabbcaa32afb922
shoito <infy2c@gmail.com>
2011-08-14 01:05:36 +0900

    プラン画面を一通り流れるようにした

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(function() {
 	function requestGetPhotos(from, to) {
 		$.ajax({
 			type: 'GET',
-			url: 'http://www.panoramio.com/map/get_panoramas.php',
+			url: 'http://www.panoramio.com/map/get_panoramas.php?callback=?',
 			data: {
 			    'set': 'public',
 			    'from': from,
@@ -19,17 +19,39 @@ $(function() {
 			    'maxy': 90,
 			    'size': 'medium',
 			    'mapfilter': true
-			    },
+			},
+            dataType: 'jsonp',
+            jsonp: 'callbackFunc',
 			success: function(data){
 				if (data != null && data.count > 1) {
+                    var currentPhoto = data.photos[0];
+                    $('#current').attr({
+                        src: currentPhoto.photo_file_url, 
+                        title: currentPhoto.photo_title,
+                        'data-photoid': currentPhoto.photo_id,
+                        width: 800,
+                        height: 600
+                    });
+                    
+                    var nextPhoto = data.photos[1];
+                    $('#next').attr({
+                        src: nextPhoto.photo_file_url, 
+                        title: nextPhoto.photo_title,
+                        'data-photoid': currentPhoto.photo_id,
+                        width: 160,
+                        height: 160
+                    });
+				
 					loadPhoto(data.photos, 0);
 				}
+			},
+			complete: function(data) {
 			}
 		});
 	}
 	
 	function loadPhoto(photos, index) {
-		if (photos.length < index + 2) {
+		if (photos.length < index + 3) {
 			var from = pageSize * ++currentPage;
 			requestGetPhotos(from, from + pageSize);
 			return;
@@ -37,41 +59,48 @@ $(function() {
 		
 		var currentPhoto = photos[index];
 		var nextPhoto = photos[index + 1];
+		var afterNextPhoto = photos[index + 2];
 
 		$('#next').click(function () {
 			$(this).unbind('click');
+			$(this).attr({
+			    src: afterNextPhoto.photo_file_url,
+			    title: afterNextPhoto.photo_title,
+			    'data-photoid': afterNextPhoto.photo_id,
+                width: 160,
+                height: 160
+			}).load(function() {
+			    $('#current').attr({
+    		        src: nextPhoto.photo_file_url, 
+    		        title: nextPhoto.photo_title,
+    		        'data-photoid': nextPhoto.photo_id,
+                    width: 800,
+                    height: 600
+    		    });
+			});
+			
 		    loadPhoto(photos, index + 1);
-		}).attr({
-		    src: nextPhoto.photo_file_url, 
-		    title: nextPhoto.photo_title,
-		    width: 160,
-		    height: 160,
-		    'data-photoid': nextPhoto.photo_id
-		});
-		
-		$('#current').attr({
-		    src: currentPhoto.photo_file_url, 
-		    title: currentPhoto.photo_title,
-			width: 800,
-			height: 600,
-		    'data-photoid': currentPhoto.photo_id
-		});
+		})
 	}
 	
 	$('#wannago').click(function() {
-		//document.location="location.html";
 		var selectedPhotoId = $('#current').attr('data-photoid');
         $.ajax({
         	type: 'GET',
-        	url: '/new',
+        	url: 'http://seren.tv/new',
         	data: {
-        	    'photo_id': photoId
+        	    'p': selectedPhotoId
         	},
         	success: function(data) {
         		console.log(data);
-        	}
-        	error: function(msg) {
-        	    console.log(msg);
+        		// UI遷移
+        	},
+        	error: function(data) {
+        	    console.log(data);
+        	    
+        	    if (data.status == 403) {
+        	        document.location = 'http://seren.tv/login';
+        	    }
         	}
         });
 	});
